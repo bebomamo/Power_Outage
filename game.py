9c0ed4ac2ccd63ab7d066118330106f883634a43
Jack <jjameson1479@gmail.com>
2022-06-05 17:49:47 -0500

    added .gitignore/requirements.txt and reformatted some stuff

--- game.py
+++ game.py
@@ -3,12 +3,12 @@ import pygame
 import os
 from HomeSelect import *
 
-view = "Home"
+# Window setup
 WIDTH, HEIGHT = 900, 500 #subject to change
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Power Outage")
-FPS = 60
-#Objects and backgrounds
+
+# Objects and backgrounds
 HomeDay = DaySelect()
 HOME_image = pygame.image.load(os.path.join('Assets', HomeDay)) #adding image
 HOME = pygame.transform.scale(HOME_image, (900, 500)) #image resizing
@@ -16,6 +16,7 @@ HOME = pygame.transform.scale(HOME_image, (900, 500)) #image resizing
 def draw_image():
     if(view == "Home"):
         WIN.blit(HOME, (0, 0)) #display home image
+    
     """elif(view == "Window"):
         WIN.blit(WINDOW, (Window.x, Window.y)) #display window image
     elif(view == "Fireplace"):
@@ -24,18 +25,32 @@ def draw_image():
         WIN.blit(FRONTDOOR, (0,0)) #display Frontdoor image
     elif(view == "Bunker"):
         WIN.blit(BUNKER, (0,0)) #display Bunker image"""
+
     pygame.display.update()
 
+# initialization
+view = "Home"
+FPS = 60
+
 def main():
+    # initialization nation
     Window = pygame.Rect(200, 50, 100, 300) #Widthpos, Heightpos, Width, Height
     clock = pygame.time.Clock()
     run = True
+    day = 0
+    
+    # stuff that happens while the game is running
     while run:
         clock.tick(FPS)
+
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
-                run = False
-        draw_image()
+                run = False # exit while loop
+        
+        draw_image() # update image every every event has been iterated through
+
+
     pygame.quit()
 
 if __name__ == "__main__":
