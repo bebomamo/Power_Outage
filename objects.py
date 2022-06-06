24274567c396cd773b5c24c459f043a557097f34
Ben Malone <benmalone390@gmail.com>
2022-06-06 14:21:49 -0500

    play button parameters and coordinate test

--- objects.py
+++ objects.py
@@ -5,3 +5,9 @@ class Window:
 # note: FrontDoor, Bunker, and Fireplace (at least) will all be added once their attributes have been precisely defined
 #       At this point, it seems like FrontDoor, Bunker, and Fireplace should all be classes and Fear should not,
 #       although this may change.
+
+#coordinate hitbox outlines
+#Play button -> top left = (378, 46)
+#            -> bottom left = (378, 460)
+#            -> bottom right = (780, 460)
+#            -> top right = (780, 46)
