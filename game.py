c0bb9323d292035a2a948e423d35a39f5cb3261f
Ben Malone <benmalone390@gmail.com>
2022-06-06 18:10:30 -0500

    clock added and load screen added

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
-from pickle import TRUE
+from pickle import FALSE, TRUE
 import pygame, os, sys, objects
+import time
 
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
@@ -12,13 +13,21 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 WIN.fill(WHITE) # set to white so we remember it exists, will almost certainly be changed later
 pygame.display.set_caption("Power Outage")
 
-# --------initialization--------
+# --------initialization nation--------
 view = "Home"
 FPS = 60
 # Objects and backgrounds
 # HomeDay = DaySelect() # - commented out for now
 HOME_image = pygame.image.load(os.path.join('assets', 'PO_night1.PNG')) #adding image
 HOME = pygame.transform.scale(HOME_image, (WIDTH, HEIGHT)) #image resizing
+LOAD_image = pygame.image.load(os.path.join('assets', 'dummy.jpg')) #adding temp loading image
+LOAD = pygame.transform.scale(LOAD_image, (WIDTH, HEIGHT)) # resizing temp load image
+FIREPLACE_unlit_open_image = pygame.image.load(os.path.join('assets', 'PO_Fireplace_unlit_open_beta.PNG')) #adding image
+FIREPLACE_unlit_open = pygame.transform.scale(FIREPLACE_unlit_open_image, (WIDTH, HEIGHT)) #image resizing
+FIREPLACE_unlit_closed_image = pygame.image.load(os.path.join('assets', 'PO_Fireplace_unlit_closed_beta.PNG')) #adding image
+FIREPLACE_unlit_closed = pygame.transform.scale(FIREPLACE_unlit_closed_image, (WIDTH, HEIGHT)) #image resizing
+FIREPLACE_lit_open_image = pygame.image.load(os.path.join('assets', 'PO_Fireplace_lit_open_beta.PNG')) #adding image
+FIREPLACE_lit_open = pygame.transform.scale(FIREPLACE_lit_open_image, (WIDTH, HEIGHT)) #image resizing
 # ------------------------------
 
 # # note: I moved DaySelect from its own file to here because it is only one function. For organization's 
@@ -46,13 +55,13 @@ HOME = pygame.transform.scale(HOME_image, (WIDTH, HEIGHT)) #image resizing
 def draw_image():
     if(view == "Home"):
         WIN.blit(HOME, (0, 0)) #display home image
-    if(view == "Game"):
-        WIN.fill(BLUE) # temporary
+    elif(view == "Game-load"):
+        WIN.blit(LOAD, (0, 0)) # temporary
 
     # elif(view == "Window"):
     #     WIN.blit(WINDOW, (Window.x, Window.y)) #display window image
-    # elif(view == "Fireplace"):
-    #     WIN.blit(FIREPLACE, (0,0)) #display Fireplace image
+    elif(view == "Fireplace-start"):
+        WIN.blit(FIREPLACE_unlit_open, (0,0)) #display Fireplace starting image
     # elif(view == "Frontdoor"):
     #     WIN.blit(FRONTDOOR, (0,0)) #display Frontdoor image
     # elif(view == "Bunker"):
@@ -67,7 +76,8 @@ def main():
     clock = pygame.time.Clock()
     day = 1 # DaySelect()
     global view
-
+    RoundStart = False
+    count = 0
     clicking = False
     right_clicking = False
 
@@ -86,10 +96,24 @@ def main():
         in_start_range_x = loc[0] >= START_BUTTON_X_MIN and loc[0] <= START_BUTTON_X_MAX
         in_start_range_y = loc[1] >= START_BUTTON_Y_MIN and loc[1] <= START_BUTTON_Y_MAX
 
-        if view == "Home" and clicking and in_start_range_x and in_start_range_y:
-            view = "Game"
-        elif view == "Game" and clicking:
+        #Round Clock - is initialized with RoundStart = True and is counted with count, count is then used for all round timings
+        #as well as object timing functions, it is also reset after 600 seconds have passed
+        if (RoundStart == True) and (count != 10): #change the count condition value to have a lower time for testing
+            count = count + 1
+            time.sleep(1)
+        else:
+            count = 0
             view = "Home"
+            RoundStart = False
+            #day would also be incremented with fileIO in this section, but for now just goes back to the home screen instead of beginning the next day sequence
+
+
+        if view == "Home" and clicking and in_start_range_x and in_start_range_y:
+            view = "Game-load"
+            RoundStart = True
+        elif view == "Game-load":
+            time.sleep(3)
+            view = 'Fireplace-start'
 
         clicking = False # one click allowed per frame - probably a temporary solution
         for event in pygame.event.get():
